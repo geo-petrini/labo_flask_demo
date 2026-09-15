09eb29292f84d42ca5610ee617efea7d7a267832
Geo Petrini <geo.petrini@gmail.com>
2026-09-15 15:22:43 +0200

    added product - category relationship

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from models.conn import db
+from models.model import seed_categories
 from flask_migrate import Migrate
 
 from blueprints.frontend import app as fe_bp
@@ -12,6 +13,15 @@ db.init_app(app)
 migrate = Migrate(app, db)
 
 app.register_blueprint(fe_bp)
+
+@app.cli.command("seed")
+def seed():
+    '''
+    use this function from command line after flask db upgrade
+    >flask seed
+    '''
+    seed_categories()
+    print("Database seeded")
 
 if __name__ == "__main__":
     app.run(debug=True)
--- models/model.py
+++ models/model.py
@@ -1,26 +1,35 @@
 from models.conn import db
 
-# class User(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     username = db.Column(db.String(80), unique=True, nullable=False)
-    
 class Product(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(120), nullable=False)
     description = db.Column(db.String(260))
     price = db.Column(db.Float, nullable=False)
+    category_id  = db.Column(db.Integer, db.ForeignKey('category.id'))
+    category = db.relationship('Category', backref=db.backref('product', lazy=True))
     
     def to_dict(self):
-        out = {
+        return {
             'id':self.id,
             'name':self.name,
             'description':self.description,
             'price':self.price,
+            'category':self.category.to_dict() if self.category else None
         }
-        # out['id'] = self.id
-        # out['name'] = self.name
-        # out['description'] = self.description
-        # out['price'] = self.price
-        return out
         
+class Category(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), nullable=False)
     
+    def to_dict(self):
+        return {
+            'id':self.id,
+            'name':self.name,
+        }
+        
+def seed_categories():
+    names = ["Electronics","Clothing","Food","Other"]
+    for name in names:
+        if not Category.query.filter_by(name=name).first():
+            db.session.add( Category(name=name) )
+    db.session.commit()
