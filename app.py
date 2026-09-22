b656c279360302f17567ea486f432e1db23b48bc
Geo Petrini <geo.petrini@gmail.com>
2026-09-22 13:38:37 +0200

    moved config to .env

--- app.py
+++ app.py
@@ -1,14 +1,20 @@
+import os
 from flask import Flask
 from models.conn import db
 from models.model import seed_categories
 from flask_migrate import Migrate
+from dotenv import load_dotenv
+
+load_dotenv()   # da eseguire prima di importare blueprints o altri moduli dell'applicativo
 
 from blueprints.frontend import app as fe_bp
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///sqlalchemy_app.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('SQLALCHEMY_DATABASE_URI')
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 
 db.init_app(app)
+# db.create_all() #creates all tables and constraints, DO NOT USE with Migrate
 
 migrate = Migrate(app, db)
 
